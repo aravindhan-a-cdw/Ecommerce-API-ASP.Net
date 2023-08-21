6ee76755314a931f66f9fc57be48555e49f6205e
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-08-21 18:29:59 +0530

    Added Mapping for DTO to Model Object and vice versa.

--- MappingConfig.cs
+++ MappingConfig.cs
@@ -1,11 +1,15 @@
 ﻿using AutoMapper;
+using EcommerceAPI.Models.DTO.User;
+using EcommerceAPI.Models;
+
 namespace EcommerceAPI
 {
 	public class MappingConfig: Profile
 	{
 		public MappingConfig()
 		{
-
+			CreateMap<UserCreateDTO, User>();
+			CreateMap<User, UserPublicDTO>();
 		}
 	}
 }
